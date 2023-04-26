c3d8e70ff510916618a0374a7397e8fa5ac73469
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2023-04-26 15:30:43 -0300

    update comment about dynamic module path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import DynamicModule from './demos/DynamicModule';
 export default function App() {
   /**
    * Aqui nós definimos o que será renderizado quando o módulo dinâmico for chamado.
-   * Se você está utilizando nossa estrutura padrão, o path precisa ser "/agendaedu/dynamicmodule".
+   * O path definido aqui precisa ser o mesmo que foi utilizado no cadastro da Agenda Edu.
    */
   return (
     <Routes>
