45771d8bb471f48d79ca823d85af95a8b3721dcd
Daniel Vieira <danielvieiraufc@gmail.com>
2024-10-08 23:36:04 -0300

    feat: redirect api url from token iss

--- src/demos/DynamicModule/index.jsx
+++ src/demos/DynamicModule/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 import './styles.css';
 
@@ -12,31 +13,52 @@ import './styles.css';
  * módulo foi chamado você poderá fazer tudo aquilo que precisar.
  */
 export default function DynamicModule() {
-  const AE_API_URL = process.env.NODE_ENV === 'production' ? 'https://api.agendaedu.com'
-                                                           : 'http://api.agendaedu.localhost:3000';
-
-  let [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const [currentUser, setCurrentUser] = useState(null);
+  const [apiUrl, setApiUrl] = useState('');
+
+  useEffect(() => {
+    const token = searchParams.get('token');
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      setApiUrl(decodedToken.iss === 'sandbox.auth_server'
+        ? 'https://sandbox-api.agendaedu.dev'
+        : process.env.NODE_ENV === 'production'
+          ? 'https://api.agendaedu.com'
+          : 'http://api.agendaedu.localhost:3000'
+      );
+    }
+  }, [searchParams]);
 
   useEffect(() => {
     async function fetchCurrentUser() {
-      const response = await fetch(`${AE_API_URL}/v2/me`, {
+      const response = await fetch(`${apiUrl}/v2/me`, {
         headers: {
           Authorization: `Bearer ${searchParams.get('token')}`
         }
       });
+
+      if (!response.ok) {
+        // Tratar erros de resposta aqui, se necessário
+        console.error('Failed to fetch user data');
+        return;
+      }
+
       const jsonData = await response.json();
       setCurrentUser(jsonData);
     }
 
-    if (!currentUser) fetchCurrentUser();
-  }, [currentUser, searchParams, AE_API_URL]);
+    if (apiUrl && !currentUser) {
+      fetchCurrentUser();
+    }
+  }, [apiUrl, currentUser, searchParams]);
 
   return (
     <>
       {currentUser && (
-        currentUser.data.attributes.role === 'responsible' ? <ResponsibleDesc responsible={currentUser} />
-                                                           : <StudentDesc student={currentUser} />
+        currentUser.data.attributes.role === 'responsible'
+          ? <ResponsibleDesc responsible={currentUser} />
+          : <StudentDesc student={currentUser} />
       )}
     </>
   );
@@ -47,17 +69,17 @@ function ResponsibleDesc({ responsible }) {
   const responsibleSchool = responsible.included.find(o => o.type === 'school');
   const responsibleStudent = responsible.included.find(o => o.type === 'family_profile');
   const studentClassroomsText = responsible.included
-        .filter(o => o.type === 'classroom')
-        .map(c => `${c.attributes.name} (${c.attributes.segment})`)
-        .join(', ');
-  
+    .filter(o => o.type === 'classroom')
+    .map(c => `${c.attributes.name} (${c.attributes.segment})`)
+    .join(', ');
+
   return (
     <div className="App">
       <p>
         Seu nome é <strong>{userAttributes.name}</strong>,
         e você possui uma conta de <strong>responsável</strong> na Agenda Edu.
       </p>
-      
+
       <p>
         Você está vinculado à escola <strong>{responsibleSchool.attributes.name}</strong>,
         onde seu/sua filho(a) <strong>{responsibleStudent.attributes.name}</strong> estuda.
@@ -70,21 +92,21 @@ function ResponsibleDesc({ responsible }) {
   );
 }
 
-function StudentDesc({ student }) {  
+function StudentDesc({ student }) {
   const userAttributes = student.data.attributes;
   const studentSchool = student.included.find(o => o.type === 'school');
   const studentClassroomsText = student.included
-        .filter(o => o.type === 'classroom')
-        .map(c => `${c.attributes.name} (${c.attributes.segment})`)
-        .join(', ');
+    .filter(o => o.type === 'classroom')
+    .map(c => `${c.attributes.name} (${c.attributes.segment})`)
+    .join(', ');
   const studentResponsibles = student.included.filter(o => o.type === 'family_profile');
-  const responsibleNames = studentResponsibles.map(r => r.attributes.name).join(', ');  
-  
+  const responsibleNames = studentResponsibles.map(r => r.attributes.name).join(', ');
+
   return (
     <div className="App">
       <p>
         Seu nome é <strong>{userAttributes.name}</strong>,
-        e você possui uma conta de <strong>estudante</strong> na Agenda Edu.     
+        e você possui uma conta de <strong>estudante</strong> na Agenda Edu.
       </p>
 
       <p>
