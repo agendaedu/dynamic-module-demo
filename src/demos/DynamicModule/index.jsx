060e885af84218260702e4e7540e69e00c49284a
Daniel Vieira <danielvieiraufc@gmail.com>
2024-10-09 10:17:54 -0300

    fix: import jwt-decode correctly

--- src/demos/DynamicModule/index.jsx
+++ src/demos/DynamicModule/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 import './styles.css';
 
